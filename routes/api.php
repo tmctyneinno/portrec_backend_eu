f99b57a0f53ed48d96b368b5f2d51a295ae3ac87
Afowowe Olumide Victor <volumide42@gmail.com>
2023-09-21 18:12:37 +0100

    create routes for login and signup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\RecruiterAuthController;
+use App\Http\Controllers\Auth\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix("user")->group(function () {
+    Route::post("signup", [UserAuthController::class, "signup"]);
+    Route::post("sigin", [UserAuthController::class, "signin"]);
+});
+
+Route::prefix("recruiter")->group(function () {
+    Route::post("signup", [RecruiterAuthController::class, "signup"]);
+    Route::post("sigin", [RecruiterAuthController::class, "signin"]);
 });
