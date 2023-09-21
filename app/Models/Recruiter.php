1f911d1c9de12fbaead9ce532ea1f07be109797a
Afowowe Olumide Victor <volumide42@gmail.com>
2023-09-21 18:07:57 +0100

    change recruiter model to authenticable

--- app/Models/Recruiter.php
+++ app/Models/Recruiter.php
@@ -3,21 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Recruiter extends Model
+class Recruiter extends Authenticatable
 {
-    use HasFactory, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
     protected $fillable = ['name', 'email', 'password', 'phone', 'location', 'recruiter_level'];
 
     protected $hidden = ['password'];
 
-
-    // public function recruiterJobs()
-    // {
-
-    // }
+    protected $casts = [
+        'password' => 'hashed',
+    ];
 
     public function recruiterSubscriptions()
     {
