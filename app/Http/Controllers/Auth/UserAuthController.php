8208065571aa0db0231d1de089706bbdddd1d407
Afowowe Olumide Victor <volumide42@gmail.com>
2023-09-21 18:11:11 +0100

    implemnt auth for user

---
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserAuthController extends AuthController
+{
+    public function signup(Request $request)
+    {
+        $req["name"] = $request->fullName;
+        $req['email'] = $request->email;
+        $req['password'] = $request->password;
+
+        $create = $this->create($req);
+        if (is_string($create) || $create['error'])
+            return $this->errorMessage($create);
+        return $this->successMessage($create, "success", 201);
+    }
+
+    public function signin(Request $request)
+    {
+        $req['email'] = $request->email;
+        $req['password'] = $request->password;
+
+        $login = $this->login($req);
+        if (is_string($login))
+            return $this->errorMessage($login);
+
+        $token = $login->createToken("portrecToken")->plainTextToken;
+        return $this->successMessage(["token" => $token], "login success");
+    }
+}
