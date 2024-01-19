d7335d6c9e40a0b53e1c01283af6eb5069f8aeb1
Michael Ozoudeh <jesmikky@gmail.com>
2024-01-19 22:21:27 +0100

    updated

--- app/Models/JobOpening.php
+++ app/Models/JobOpening.php
@@ -9,4 +9,41 @@ class JobOpening extends Model
 {
     use HasFactory;
     protected $fillable = ['recruiter_id', 'company_id', 'job_level_id', 'country_id', 'job_function_id', 'job_type_id', 'title', 'description', 'required_skills', 'min_salary', 'max_salary', 'deadline', 'qualifications', 'location', 'experience', 'total_view', 'other_qualifications', 'benefits', 'status', 'responsibilities', 'capacity', 'total_applied'];
+
+    protected $hidden = [
+        "recruiter_id",
+        "company_id",
+        "job_type_id",
+        "job_functions"
+    ];
+
+    public function jobType()
+    {
+        return $this->belongsTo(JobType::class, "job_type_id", "id");
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function recruiter()
+    {
+        return $this->belongsTo(Recruiter::class);
+    }
+
+    public function level()
+    {
+        return $this->belongsTo(JobLevel::class);
+    }
+
+    public function sub_category()
+    {
+        return $this->belongsTo(JobFunction::class, "job_functions", "id");
+    }
+
+
+
+
+
 }
